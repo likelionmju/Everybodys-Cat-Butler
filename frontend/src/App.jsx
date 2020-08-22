5c3bfaae22a5d4e93947ceeb94e97661acdc8d36
AiRini <ygnaiih1680@mju.ac.kr>
2020-08-23 02:05:04 +0900

    add kakao map

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,28 +1,41 @@
-//frontend/src/app.js
-import React, { Component } from 'react';
+/*global kakao*/
+//frontend/src/app.jsx
+import React, {Component} from 'react';
 import axios from 'axios'
+
+//css
+import './App.css'
+
+const baseURL = axios.defaults.baseURL = 'http://127.0.0.1:8000/api/'
 axios.defaults.withCredentials = true
-axios.defaults.baseURL = 'http://127.0.0.1:8000/api/'
 
 class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            posts: []
-        };
-    }
-
+    state = {
+        posts: []
+    };
 
     async componentDidMount() {
-        axios.get(axios.defaults.baseURL)
-            .then(({data}) => this.setState({ posts: data }))
-            .catch(e => console.log(e))
+        axios.get(baseURL)
+            .then((res) => this.setState({posts: res.data}))
+            .catch((e) => console.log(e))
+        const mapScript = document.getElementById('kakao-map')
+        mapScript.addEventListener('load', () => {
+            kakao.maps.load(() => {
+                const container = document.getElementById('map-container');
+                const mapOption = {
+                    center: new kakao.maps.LatLng(37.566826, 126.9786567),
+                    level: 3
+                };
+
+                const map = new kakao.maps.Map(container, mapOption);
+            })
+        });
     }
 
     render() {
         return (
             <div>
-                {this.state.posts}
+                <div id="map-container"/>
                 {this.state.posts.map(item => (
                     <div key={item.id}>
                         <h1>{item.title}</h1>
