54477c8be833b9fa6312729e1372c4792e3fe5c7
AiRini <ygnaiih1680@mju.ac.kr>
2020-08-23 00:36:24 +0900

    reformat code

--- backend/reactapi/settings.py
+++ backend/reactapi/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
@@ -27,7 +26,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -37,20 +35,20 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'post', # 백엔드 app
-    'rest_framework', # 장고 REST 프레임워크
-    'corsheaders', # HTTP 접근제어 규약
+    'post',  # 백엔드 app
+    'rest_framework',  # 장고 REST 프레임워크
+    'corsheaders',  # HTTP 접근제어 규약
 ]
 
-REST_FRAMEWORK = {                      #장고 REST 프레임워크
+REST_FRAMEWORK = {  # 장고 REST 프레임워크
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.AllowAny',
     ]
 }
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',     # HTTP 접근제어 규약
-    'django.middleware.common.CommonMiddleware', # HTTP 접근제어 규약
+    'corsheaders.middleware.CorsMiddleware',  # HTTP 접근제어 규약
+    'django.middleware.common.CommonMiddleware',  # HTTP 접근제어 규약
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -80,7 +78,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'reactapi.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
@@ -90,7 +87,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
@@ -110,7 +106,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
@@ -123,7 +118,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
